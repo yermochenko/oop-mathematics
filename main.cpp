4e025fc76c178b8b31daaa84d3b59d5271e8056c
Sergey Yermochenko <yermochenko@gmail.com>
2016-11-30 13:30:38 +0300

    Исправлены ошибки объявления переменных и убраны вызовы system("pause")

--- main.cpp
+++ main.cpp
@@ -28,18 +28,17 @@ int main()
 	fs.close();
 	//Метод Жердана-Гаусса
 	//Прямой ход, приведение к верхнетреугольному виду
-	int k;
-	double  f, f1;
-	for (i = 0; i<n; i++)
+	double  f, f1, g;
+	for (int i = 0; i<n; i++)
 	{
 		// проверка на 0
 		if (A[i][i] == 0)
 		{
-			for (l = i + 1; l<n; l++)
+			for (int l = i + 1; l<n; l++)
 			{
 				if (A[l][i] != 0)
 				{
-					for (t = 0; t<m; t++)
+					for (int t = 0; t<m; t++)
 					{
 						f1 = A[i][t];
 						A[i][t] = A[l][t];
@@ -48,32 +47,31 @@ int main()
 				}
 				else{
 					cout << "Нет единственного решения" << endl;
-					system("pause");
 					return 0;
 				}
 			}
 		}//Конец проверки на 0
 		f = A[i][i];
 
-		for (j = 0; j<m; j++)
+		for (int j = 0; j<m; j++)
 		{
 			A[i][j] /= f;
 		}
-		for (h = 0; h<n; h++)
+		for (int h = 0; h<n; h++)
 		{
 			if (h != i)
 			{
 
 				g = A[h][i];
 
-				for (k = 0; k<m; k++)
+				for (int k = 0; k<m; k++)
 				{
 					A[h][k] = -g*A[i][k] + A[h][k];//делаем нули
 
 				}
 
 			}
-			if (j == i) continue;
+			//if (j == i) continue;
 		}
 	}
 
@@ -83,7 +81,7 @@ int main()
 
 	for (int i = 0; i<n; i++)
 	{
-		for (j = 0; j<m; j++){
+		for (int j = 0; j<m; j++){
 			fs >> A[i][j];
 			cout << "  " << A[i][j];
 		}
@@ -91,9 +89,8 @@ int main()
 	}
 	//Выводим решение
 
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		cout << "x[" << i << "] = " << floor(A[i][m - 1] * 1000) / 1000 << " " << endl;
 	cout << endl;
-	system("pause");
 	return 0;
-}ю.
+}
