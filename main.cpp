fa6cdeb7c79b2e023c2de25566555ec70e662b29
User <User@TOSHIBA>
2016-12-08 18:33:19 +0300

    Рефакторинг Замена цикла for для обмена двух строк матрицы на класс For

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ class Problem
 public:
 	Matrix* matrix;
 	bool error;
-	int i, k;
+	int i, j, k;
 };
 
 class ErrorCondition: public Condition
@@ -37,6 +37,64 @@ public:
 	}
 };
 
+class ExchangeRowsElementInitStatement: public Statement
+{
+	Problem *problem;
+public:
+	ExchangeRowsElementInitStatement(Problem *problem)
+	{
+		this->problem = problem;
+	}
+	void execute()
+	{
+		problem->j = 0;
+	}
+};
+
+class ExchangeRowsElementCondition: public Condition
+{
+	Problem *problem;
+public:
+	ExchangeRowsElementCondition(Problem *problem)
+	{
+		this->problem = problem;
+	}
+	bool check()
+	{
+		return problem->j < problem->matrix->colsCount();
+	}
+};
+
+class ExchangeRowsElementEndIterationStatement: public Statement
+{
+	Problem *problem;
+public:
+	ExchangeRowsElementEndIterationStatement(Problem *problem)
+	{
+		this->problem = problem;
+	}
+	void execute()
+	{
+		problem->j++;
+	}
+};
+
+class ExchangeRowsElementBodyStatement: public Statement
+{
+	Problem *problem;
+public:
+	ExchangeRowsElementBodyStatement(Problem *problem)
+	{
+		this->problem = problem;
+	}
+	void execute()
+	{
+		double f1 = problem->matrix->get(problem->i, problem->j);
+		problem->matrix->set(problem->i, problem->j, problem->matrix->get(problem->k, problem->j));
+		problem->matrix->set(problem->k, problem->j, f1);
+	}
+};
+
 class NonZeroElementCondition: public Condition
 {
 	Problem *problem;
@@ -54,21 +112,33 @@ public:
 class NonZeroElementStatement: public Statement
 {
 	Problem *problem;
+	Statement *exchangeRowsElementInitStatement;
+	Condition *exchangeRowsElementCondition;
+	Statement *exchangeRowsElementEndIterationStatement;
+	Statement *exchangeRowsElementBodyStatement;
+	For *exchangeRowsElementFor;
 public:
 	NonZeroElementStatement(Problem *problem)
 	{
 		this->problem = problem;
+		exchangeRowsElementInitStatement = new ExchangeRowsElementInitStatement(problem);
+		exchangeRowsElementCondition = new ExchangeRowsElementCondition(problem);
+		exchangeRowsElementEndIterationStatement = new ExchangeRowsElementEndIterationStatement(problem);
+		exchangeRowsElementBodyStatement = new ExchangeRowsElementBodyStatement(problem);
+		exchangeRowsElementFor = new For(exchangeRowsElementInitStatement, exchangeRowsElementCondition, exchangeRowsElementEndIterationStatement, exchangeRowsElementBodyStatement);
 	}
 	void execute()
 	{
 		problem->error = false;
-		double f1;
-		for (unsigned int j = 0; j < problem->matrix->colsCount(); j++)
-		{
-			f1 = problem->matrix->get(problem->i, j);
-			problem->matrix->set(problem->i, j, problem->matrix->get(problem->k, j));
-			problem->matrix->set(problem->k, j, f1);
-		}
+		exchangeRowsElementFor->execute();
+	}
+	~NonZeroElementStatement()
+	{
+		delete exchangeRowsElementFor;
+		delete exchangeRowsElementInitStatement;
+		delete exchangeRowsElementCondition;
+		delete exchangeRowsElementEndIterationStatement;
+		delete exchangeRowsElementBodyStatement;
 	}
 };
 
